b30acd87b731331d11abfaa890f49ab7b3ad2e37
geekpantura <geekpantura@gmail.com>
2016-10-11 21:41:05 +0700

    Add slug to Category

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    categories = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': categories}
 
     return render(request, 'rango/index.html', context=context_dict)
 
